0b5d8c47c7c6339298bb13948f74d7df3fdaea33
Kamshat <komaliyeva@gmail.com>
2022-12-07 23:25:52 +0600

    готовые

--- hello/urls.py
+++ hello/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
-from .views import index, hello
+from .views import index, hello, base
 
 urlpatterns = [
     re_path(r'^index/$', index,  name="index"),
-    path("hello/", hello, kwargs={"name": "Tom", "age": 15}, name="hello")
+    path("hello/", hello, kwargs={"name": "Tom", "age": 15}, name="hello"),
+    path("base/", base)
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 
 
 def index(request):
@@ -9,3 +10,10 @@ def index(request):
 def hello(request, name, age):
     return HttpResponse(f"<h1>{name}, </h1> "
                         f"<h3>{age}</h3>")
+
+
+def base(request):
+    hobbies = {"tom": "swim", "bob": "football", "martin": "None"}
+    animals = ["Monkey", "elephant", "cat", "dog"]
+    data = {"hobbies": hobbies, "animals": animals}
+    return TemplateResponse(request, "index.html", context=data)
